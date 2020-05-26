a77fab8f0bf81cd3bf03b70dc1db8b5d6912fd09
martynasd123 <dargis.martynas@gmail.com>
2020-05-26 20:28:52 +0300

    API places with google API

--- src/pages/ApiPlaces.js
+++ src/pages/ApiPlaces.js
@@ -7,6 +7,8 @@ import debounce from 'lodash/debounce'
 import Masonry from "react-masonry-component";
 import SearchInputComponent from "../components/SearchInputComponent";
 import Strings from "../helpers/stringResources";
+import Typography from "@material-ui/core/Typography";
+import UseSnackbarContext from "../contexts/UseSnackbarContext";
 
 
 
@@ -38,41 +40,23 @@ function ApiPlaces({classes}){
 
     const [data, setData] = useState([]);
     const [isLoadingData, setIsLoadingData] = useState(false);
-    const [after, setAfter] = useState(null);
-    const [searchQuery, setSearchQuery] = useState("Kaunas");
+    const { addConfig } = UseSnackbarContext();
 
-    const loadDataFunc = (query, after) => {
-        // console.log(data)
+    const loadDataFunc = (query) => {
+        setData([])
         setIsLoadingData(true)
-        API.Places.searchApiPlaces("?keyword=" + encodeURI(query) + (after != null ? "&after=" + after : "")).then(response => {
-            setData(currentData => [...currentData, ...(response.data)])
-            setAfter(response.after)
+        API.Places.searchApiPlaces("?keyword=" + encodeURI(query)).then(response => {
+            setData([...response])
         }).catch(error => {
+            addConfig(false, "The request did not go through.")
         }).finally(() => {
             setIsLoadingData(false)
         })
     }
 
-    const handleScrollDebounced = (e) => {
-        const scrollOffset = 100 //px
-        const bottom = Math.round(e.target.scrollHeight - e.target.scrollTop - scrollOffset)  <= e.target.clientHeight;
-        if (bottom && !isLoadingData) {
-            loadDataFunc(
-                searchQuery, after)
-        }
-    }
-
-    const debounceScroll = debounce(e => handleScrollDebounced(e), 200)
-
-    const handleScroll = (e) => {
-        e.persist()
-        debounceScroll(e)
-    }
-
-    const handleSearch = (query) => {
-        setData([])
-        loadDataFunc(searchQuery, null)
-        setSearchQuery(query)
+    const handleSearch = (word) => {
+        if(word.length > 3)
+            loadDataFunc(word)
     }
 
     return(
@@ -81,7 +65,7 @@ function ApiPlaces({classes}){
                 <SearchInputComponent searchCallback={handleSearch} hint="Search api places" style={classes.searchBox} />
             </div>
 
-            <div className={classes.root} onScroll={(e) => handleScroll(e)}>
+            <div className={classes.root}>
                 <Masonry
                     // className={'my-gallery-class'} // default ''
                     style={{margin: "0 auto"}}
@@ -97,11 +81,19 @@ function ApiPlaces({classes}){
                     {data.map(item => {
                         return <ApiPlaceCard key={item.placeId} placeData = {item} style={{boxSizing: 'border-box'}}/>
                     })}
+
+                    {
+                        data.length === 0 && !isLoadingData &&
+                        <Typography variant="subtitle1">Vietų nerasta</Typography>
+                    }
+                    {
+                        isLoadingData &&
+                        <CircularProgress  />
+                    }
+
                 </Masonry>
-                {
-                    isLoadingData &&
-                    <CircularProgress  />
-                }
+
+
             </div>
         </div>
     )

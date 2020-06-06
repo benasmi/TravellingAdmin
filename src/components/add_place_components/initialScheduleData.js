483cdd11fdd21065c5b3304eaf8247e3fdc2266a
martynasd123 <dargis.martynas@gmail.com>
2020-06-06 13:42:53 +0300

    changed initial schedule data

--- src/components/add_place_components/initialScheduleData.js
+++ src/components/add_place_components/initialScheduleData.js
@@ -6,43 +6,31 @@ const initialScheduleData = [
                 openDay: 0,
                 closeDay: 0,
                 openTime: "08:00",
-                closeTime: "18:00"
+                closeTime: "17:00"
             },
             {
                 openDay: 1,
                 closeDay: 1,
                 openTime: "08:00",
-                closeTime: "18:00"
+                closeTime: "17:00"
             },
             {
                 openDay: 2,
                 closeDay: 2,
                 openTime: "08:00",
-                closeTime: "18:00"
+                closeTime: "17:00"
             },
             {
                 openDay: 3,
                 closeDay: 3,
                 openTime: "08:00",
-                closeTime: "18:00"
+                closeTime: "17:00"
             },
             {
                 openDay: 4,
                 closeDay: 4,
                 openTime: "08:00",
-                closeTime: "18:00"
-            },
-            {
-                openDay: 5,
-                closeDay: 5,
-                openTime: "08:00",
-                closeTime: "12:00"
-            },
-            {
-                openDay: 6,
-                closeDay: 6,
-                openTime: "08:00",
-                closeTime: "12:00"
+                closeTime: "17:00"
             }
         ]
     }

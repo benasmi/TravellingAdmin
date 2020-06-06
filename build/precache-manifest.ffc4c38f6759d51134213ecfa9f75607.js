b87ad04b14f88d1a7b6b955b44de19d90dedd888
martynasd123 <dargis.martynas@gmail.com>
2020-06-06 13:40:14 +0300

    dropzone area file size increased

---
+++ build/precache-manifest.ffc4c38f6759d51134213ecfa9f75607.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af70b73dbddb1a6340fba0e97c46c462",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bfe74a4597ff530623b",
+    "url": "/static/js/2.553529da.chunk.js"
+  },
+  {
+    "revision": "304e1e74f65c5db48f063764eb41bdd1",
+    "url": "/static/js/2.553529da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0348627612503b03acd0",
+    "url": "/static/js/main.1a35afb6.chunk.js"
+  },
+  {
+    "revision": "66b499e88ab057fd02a3",
+    "url": "/static/js/runtime-main.792bb06c.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f37403dd4459a6d3fa5003bde4fd7e4.js"
+  "/precache-manifest.ffc4c38f6759d51134213ecfa9f75607.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
